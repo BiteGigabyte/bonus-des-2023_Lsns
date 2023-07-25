fbbd673864d538d2121ffc878db57ed19233612e
feden2906 <feden2906@gmail.com>
2023-07-25 21:46:22 +0300

    connect to db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserController } from './user/user.controller';
 import { UserModule } from './user/user.module';
 import { AnimalController } from './animal/animal.controller';
 import { AnimalModule } from './animal/animal.module';
-import {UserService} from "./user/user.service";
+import { UserService } from './user/user.service';
+import { TypeOrmConfiguration } from './config/database/type-orm-configuration';
 
 @Module({
-  imports: [UserModule, AnimalModule],
+  imports: [
+    TypeOrmModule.forRootAsync(TypeOrmConfiguration.config),
+    UserModule,
+    AnimalModule,
+  ],
   controllers: [AppController, UserController, AnimalController],
   providers: [AppService, UserService],
 })
